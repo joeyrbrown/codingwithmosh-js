b9c0695fb2029ca92e072341aa1d9d5c72a9e0d0
Joey R Brown <joey_r_brown@homedepot.com>
2019-05-17 17:32:25 -0500

    Variables best practices within ES6.

--- index.js
+++ index.js
@@ -2,3 +2,28 @@
 
 // Explain Why's and Hows in Comments
 console.log('Hello World');
+
+// Going for in ES6 use the let keyword for declaring variables.
+let name = 'Joey';
+console.log(name);
+
+/* 
+If variables are NOT initialized with a value it will be 'undefined'.
+
+Rules for Variables
+    1. Cannot be a reserved word
+    2. Should be meaningful names
+    3. Cannot start with a number (1name)
+    4. Cannot contain space for Hypen (-)
+    5. Variables are case-sensitive
+
+Modern Best-practices are variables on one line and initializing with single quotes ''.
+*/
+
+/* CONSTANTS -
+
+
+*/
+const interestRate = 0.3;
+
+console.log(interestRate);
